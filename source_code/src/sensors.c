eb50d68eb36b43bd38034e6d51f128b2874707ab
robotaleh <alex.santos.ete@gmail.com>
2024-07-25 21:01:15 +0200

    Mejora calibración de sensores frontales para diagonales

--- source_code/src/sensors.c
+++ source_code/src/sensors.c
@@ -608,8 +608,8 @@ int16_t get_front_sensors_angle_error(void) {
 }
 
 int16_t get_front_sensors_diagonal_error(void) {
-  int16_t left_error = sensors_distance[SENSOR_FRONT_LEFT_WALL_ID] - 280;
-  int16_t right_error = sensors_distance[SENSOR_FRONT_RIGHT_WALL_ID] - 280;
+  int16_t left_error = sensors_distance[SENSOR_FRONT_LEFT_WALL_ID] - 346;
+  int16_t right_error = sensors_distance[SENSOR_FRONT_RIGHT_WALL_ID] - 346;
   // printf("\t\t%4d - %4d\n", left_error, right_error);
 
   if (right_error < 0) {
