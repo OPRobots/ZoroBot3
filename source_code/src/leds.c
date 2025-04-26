8c4640217b3ebc18f418d14dc6d8d73a767e17fb
robotaleh <alex.santos.ete@gmail.com>
2025-04-26 14:08:12 +0200

    Añade ajuste para que no se apague el RGB al establecer un color si ya había un rgbWhileMs pendiente

--- source_code/src/leds.c
+++ source_code/src/leds.c
@@ -51,6 +51,7 @@ void set_RGB_color(uint32_t r, uint32_t g, uint32_t b) {
   timer_set_oc_value(TIM1, TIM_OC3, r);
   timer_set_oc_value(TIM1, TIM_OC4, b);
   timer_set_oc_value(TIM1, TIM_OC2, g);
+  rgbWhileMs = 0;
 }
 
 void set_RGB_color_while(uint32_t r, uint32_t g, uint32_t b, uint32_t ms) {
