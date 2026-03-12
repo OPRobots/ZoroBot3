429f18838bba24d796068e411affb60ca9473efe
robotaleh <alex.santos.ete@gmail.com>
2026-03-12 22:11:58 +0100

    Añade EXPLORE_TYPE a mmsim.c

--- source_code/lib/mmsim_api/mmsim.c
+++ source_code/lib/mmsim_api/mmsim.c
@@ -9,12 +9,13 @@
 
 #include "mmsim_api.h"
 
+int MMSIM_EXPLORE_TYPE = EXPLORE_SIMPLE;
 int MMSIM_FLOODFILL_TYPE = FLOODFILL_TYPE_BASIC;
 
 int main(int argc, char *argv[]) {
 
-  for (int i = 1; i < argc; ++i) {
-    if (strncmp(argv[i], "-floodfill-type=", 6) == 0 || strncmp(argv[i], "--floodfill-type=", 7) == 0) {
+  for (int i = 1; i < argc; i++) {
+    if (strncmp(argv[i], "-floodfill-type=", 16) == 0 || strncmp(argv[i], "--floodfill-type=", 17) == 0) {
       const char *val = strchr(argv[i], '=') + 1;
       if (val) {
         switch ((int)strtol(val, NULL, 10)) {
@@ -32,6 +33,26 @@ int main(int argc, char *argv[]) {
             break;
           default:
             fprintf(stderr, "Invalid floodfill type: %s\n", val);
+            fflush(stderr);
+            exit(EXIT_FAILURE);
+            break;
+        }
+      }
+    } else if (strncmp(argv[i], "-explore-type=", 14) == 0 || strncmp(argv[i], "--explore-type=", 15) == 0) {
+      const char *val = strchr(argv[i], '=') + 1;
+      if (val) {
+        switch ((int)strtol(val, NULL, 10)) {
+          case EXPLORE_SIMPLE:
+            MMSIM_EXPLORE_TYPE = EXPLORE_SIMPLE;
+            break;
+          case EXPLORE_HOME:
+            MMSIM_EXPLORE_TYPE = EXPLORE_HOME;
+            break;
+          case EXPLORE_COMPLETE:
+            MMSIM_EXPLORE_TYPE = EXPLORE_COMPLETE;
+            break;
+          default:
+            fprintf(stderr, "Invalid explore type: %s\n", val);
             fflush(stderr);
             exit(EXIT_FAILURE);
             break;
