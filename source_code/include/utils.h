08d31cd7400bd539c9e39a042afe3f56d1655e50
robotaleh <alex.santos.ete@gmail.com>
2026-05-31 15:57:10 +0200

    Mueve tabla ln_lookup a utils.c

--- source_code/include/utils.h
+++ source_code/include/utils.h
@@ -3,6 +3,12 @@
 
 #include <stdint.h>
 
+#include "constants.h"
+
+#define LOG_LINEARIZATION_TABLE_STEP 4
+#define LOG_LINEARIZATION_TABLE_SIZE (ADC_RESOLUTION / LOG_LINEARIZATION_TABLE_STEP)
+
+float get_ln_value(uint16_t index);
 float map(float x, float in_min, float in_max, float out_min, float out_max);
 float constrain(float x, float min, float max);
 
